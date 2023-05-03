c6f7c7da17d064a1c3f03c228150bcd310065376
Deepak Nathani <deepakn1019@gmail.com>
2023-05-03 15:29:57 -0700

    add gsm prompts - missing step, variable naming

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from time import time
 import traceback
 
 class Prompt:
@@ -22,6 +23,27 @@ class Prompt:
             f"{prompt}{self.question_prefix}{question}{self.intra_example_sep}{self.answer_prefix}"
         )
 
+class LLMFeedback(Prompt):
+    def __init__(
+        self, 
+        question_prefix: str,
+        answer_prefix: str,
+        intra_example_sep: str,
+        inter_example_sep: str,
+        engine: str = None,
+        temperature: float = None,
+        max_tokens: int = 300,
+    ) -> None:
+        super().__init__(
+            question_prefix=question_prefix,
+            answer_prefix=answer_prefix,
+            intra_example_sep=intra_example_sep,
+            inter_example_sep=inter_example_sep,
+            engine=engine,
+            temperature=temperature,
+        )
+        self.instruction = """There is an error in the code above because of lack of understanding of the question. What is the error? To find the error, go through semantically complete blocks of the code, and check if everything looks good."""
+        self.max_tokens = max_tokens
 
 def retry_parse_fail_prone_cmd(
     func,
@@ -42,6 +64,7 @@ def retry_parse_fail_prone_cmd(
 
                 retries -= 1
                 print(f"An error occurred: {e}. {stack_trace}. Left retries: {retries}.")
+                time.sleep(60)
         return None
 
     return wrapper
